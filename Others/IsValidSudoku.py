d16b6d4716fb88740af0c0f2b80a46e9429efdef
iodapro <borys.knysh@gmail.com>
2021-03-02 21:29:21 +0100

    Create IsValidSudoku.py

---
+++ Others/IsValidSudoku.py
@@ -0,0 +1,34 @@
+class Solution:
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+        hashTemp = defaultdict(bool)
+        length = len(board)
+        
+        for i in range(length):
+            for j in range(length):
+                if board[i][j] != '.':
+                    if board[i][j] in hashTemp:
+                        print('What?')
+                        return False
+                    hashTemp[board[i][j]] = True
+            hashTemp = defaultdict(bool)
+        
+        for i in range(length):
+            for j in range(length):
+                if board[j][i] != '.':
+                    if board[j][i] in hashTemp:
+                        print('Whae?')
+                        return False
+                    hashTemp[board[j][i]] = True
+            hashTemp = defaultdict(bool)
+        for qi in range(3):
+            for qj in range(3):
+                for i in range(3*qi, 3*(qi+1), 1):
+                    for j in range(3*qj, 3*(qj+1), 1):
+                        if board[j][i] != '.':
+                            if board[j][i] in hashTemp:
+                                print('Whae?')
+                                return False
+                            hashTemp[board[j][i]] = True
+                hashTemp = defaultdict(bool)
+        
+        return True
