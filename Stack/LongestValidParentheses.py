54a980c58104344f9be689164ccf98be99c97e7b
iodapro <borys.knysh@gmail.com>
2021-03-11 19:07:06 +0100

    Create LongestValidParentheses.py

---
+++ Stack/LongestValidParentheses.py
@@ -0,0 +1,15 @@
+class Solution:
+    def longestValidParentheses(self, s: str) -> int:
+        stack = [-1]
+        max_length = 0
+        length = len(s)
+        for i in range(length):
+            if s[i] == '(':
+                stack.append(i)
+            else:
+                stack.pop()
+                if len(stack) == 0:
+                    stack.append(i)
+                else:
+                    max_length = max(max_length, i-stack[-1])
+        return max_length
